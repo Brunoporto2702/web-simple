4dc96cf17d614040b5a88eabca967b8671fe5587
Brunoporto2702 <bruno.porto@nubank.com.br>
2024-04-23 22:35:27 -0300

    try to move backend config

--- main.tf
+++ main.tf
@@ -3,39 +3,49 @@ provider "aws" {
   region = var.region
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-web-simple"
+    key            = "services/web-simple"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
 resource "aws_security_group" "allow_web_ssh" {
   name        = "allow_web_ssh"
   description = "Allow Web and SSH inbound traffic"
 
   ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -44,8 +54,8 @@ resource "aws_security_group" "allow_web_ssh" {
 }
 
 resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami             = var.ami_id
+  instance_type   = var.instance_type
   security_groups = [aws_security_group.allow_web_ssh.name]
 
   tags = {
