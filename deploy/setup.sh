c3f370c783b9b51ab498e2d9a09e9d9f97c8b149
Brunoporto2702 <bruno.porto@nubank.com.br>
2024-04-23 23:24:35 -0300

    update EC2 config

---
+++ deploy/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Log file location
+LOG_FILE="/var/log/docker-installation.log"
+
+{
+	# Update the system
+	sudo yum update -y
+
+	# Install git
+	Sudo yum install git -y
+
+	# Install Docker
+	sudo yum install docker -y
+
+	# Start Docker and enable it to run at startup
+	sudo service docker start
+	sudo systemctl enable docker
+
+	# Add the current user to the docker group to run Docker without sudo
+	sudo usermod -a -G docker $USER
+
+	# Install Docker Compose
+	sudo curl -L "https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+
+	# Check Docker and Docker Compose versions
+	docker --version
+	docker-compose --version
+
+	echo "Docker and Docker Compose have been installed successfully."
+} >>$LOG_FILE 2>&1
--- variables.tf
+++ variables.tf
@@ -13,3 +13,8 @@ variable "instance_type" {
   description = "The type of EC2 instance to create"
   default     = "t2.micro"
 }
+
+variable "key_pair_name" {
+  description = "The name of the EC2 key pair"
+  default     = "web-simple"
+}
